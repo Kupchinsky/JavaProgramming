926c918d6436d596b93ea32e87fd743cc4f9b866
Kupchinsky <user@killer666.ru>
2015-11-12 09:29:36 +0700

    - Updated

--- Apteka/src/main/java/sample/Main.java
+++ Apteka/src/main/java/sample/Main.java
@@ -2,8 +2,22 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,8 +25,56 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Аптека \"Рашнаптек груп\"");
+
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        final ImageView imv = new ImageView();
+        final Image image2 = new Image(Main.class.getResourceAsStream("button.jpg"));
+        imv.setImage(image2);
+
+        final HBox pictureRegion = new HBox();
+
+        pictureRegion.getChildren().add(imv);
+        grid.add(pictureRegion, 1, 0);
+
+        Text scenetitle = new Text(primaryStage.getTitle());
+        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        grid.add(scenetitle, 0, 0);
+
+        Label userName = new Label("Пользователь:");
+        grid.add(userName, 0, 1);
+
+        TextField userTextField = new TextField();
+        grid.add(userTextField, 1, 1);
+
+        Label pw = new Label("Пароль:");
+        grid.add(pw, 0, 2);
+
+        PasswordField pwBox = new PasswordField();
+        grid.add(pwBox, 1, 2);
+
+        Button btn = new Button("Войти");
+        HBox hbBtn = new HBox(10);
+        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn.getChildren().add(btn);
+        grid.add(hbBtn, 1, 4);
+
+        final Text actiontarget = new Text();
+        grid.add(actiontarget, 1, 6);
+
+        btn.setOnAction(e -> {
+            actiontarget.setFill(Color.FORESTGREEN);
+            actiontarget.setText("Sign in button pressed");
+        });
+
+        Scene scene = new Scene(grid, 550, 500);
+        primaryStage.setScene(scene);
+
         primaryStage.show();
     }
 
